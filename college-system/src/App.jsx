dd730c20388d60cde5265521123305f04ed4a6cf
Abdelrhman <lito2182002@gmail.com>
2025-10-19 21:59:35 +0300

    app creation

---
+++ college-system/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-800">College System</h1>
+      
+    </>
+  )
+}
+
+export default App
