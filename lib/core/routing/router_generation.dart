bcc4aa25cd90fc0216a2df357983960d59e67074
Ahmed Kabary <71717647+KaboOA@users.noreply.github.com>
2026-01-05 21:02:56 +0200

    feat: Remove courses and registration features from the project

--- lib/core/routing/router_generation.dart
+++ lib/core/routing/router_generation.dart
@@ -2,10 +2,7 @@ import 'package:college_project/core/routing/app_routes.dart';
 import 'package:college_project/features/auth/login_screen.dart';
 import 'package:college_project/features/splash/splash_screen.dart';
 import 'package:college_project/features/student_id/student_id_screen.dart';
-<<<<<<< HEAD
 import 'package:college_project/layout/home_layout.dart';
-=======
->>>>>>> f8d29fad350b9cd6f933c21164036dc581573381
 import 'package:go_router/go_router.dart';
 
 class RouterGenerationConfig {
