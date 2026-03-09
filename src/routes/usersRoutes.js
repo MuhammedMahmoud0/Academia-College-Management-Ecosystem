476d30997d9852b6228dfb0f3f9b05c97c191905
ZIZO-ELKHAT_TEB <CDS.ZiadMohamed23135@alexu.edu.eg>
2026-03-09 17:17:33 +0200

    display users at userManagement
    display all kind of users at  userManagement page (students - doctors - teaching_assistant)

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,25 +1,39 @@
 import express from "express";
 import {
-    getUsers,
-    addUsers,
-    addExcelUsers,
+  getUsers,
+  addUsers,
+  addExcelUsers,
+  getStudentsForManagement,
+  getStaffForManagement,
 } from "../controllers/usersController.js";
 import {
-    authMiddleware,
-    authorizationMiddleware,
+  authMiddleware,
+  authorizationMiddleware,
 } from "../middlewares/authMiddleware.js";
 import { uploadExcel } from "../middlewares/uploadMiddleware.js";
 
 const router = express.Router();
 
+router.get(
+  "/users/management/students",
+  authMiddleware,
+  authorizationMiddleware("super_admin", "admin"),
+  getStudentsForManagement,
+);
+router.get(
+  "/users/management/staff",
+  authMiddleware,
+  authorizationMiddleware("super_admin", "admin"),
+  getStaffForManagement,
+);
 router.get("/users", authMiddleware, getUsers);
 router.post("/users", authMiddleware, addUsers);
 router.post(
-    "/users/upload-excel",
-    authMiddleware,
-    authorizationMiddleware("super_admin", "admin"),
-    uploadExcel.single("file"),
-    addExcelUsers
+  "/users/upload-excel",
+  authMiddleware,
+  authorizationMiddleware("super_admin", "admin"),
+  uploadExcel.single("file"),
+  addExcelUsers,
 );
 
 export default router;
